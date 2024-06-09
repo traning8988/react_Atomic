a910189d6c472487ed8abbbbcd768d3e797d8101
Shota Nakamichi <s02178906@gmail.com>
2024-06-10 00:00:45 +0900

    atom end

---
+++ src/components/atom/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const BaseButton = styled.button`
+  color: #fff;
+  border-radius: 9999px;
+  border: none;
+  padding: 6px 24px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+  `;
---
+++ src/components/atom/button/PrimaryButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+import { BaseButton } from './BaseButton';
+
+export const PrimaryButton = (props) => {
+  const { children } = props;
+  return (
+    <SButton>{children}</SButton>
+  );
+};
+
+const SButton = styled(BaseButton)`
+  background: #40514e;
+`;
