7b43691cf772b9990ba8e6219ac15619b49a2ef2
Shota Nakamichi <s02178906@gmail.com>
2024-06-10 13:52:46 +0900

    Molecule end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import "./App.css"
 import { PrimaryButton } from './components/atom/button/PrimaryButton';
 import { SecondaryButton } from './components/atom/button/SecondaryButton';
-
+import { SubButton } from './components/atom/button/SubButton';
+import { SearchInput } from './components/Molecules/SearchInput';
 
 export default function App() {
   return (
     <div className='App'>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>うんこ</SecondaryButton>
+      <SubButton>サブボタン</SubButton>
+      <br />
+      <SearchInput></SearchInput>
     </div>
   );
 };
