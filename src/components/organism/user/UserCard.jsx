f029ecc8bd37da331410cb519e2bfb27cd2e38cf
Shota Nakamichi <s02178906@gmail.com>
2024-06-11 00:15:57 +0900

    Organism end

---
+++ src/components/organism/user/UserCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { styled } from 'styled-components'
+import { Card } from '../../atom/card/Card';
+import { UserIconWithName } from '../../Molecules/user/UserIconWithName';
+
+export const UserCard = (props) => {
+  const { user } = props;
+
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.tel}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>WEB</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+const SDl = styled.dl`
+  text-aline: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: brake-word;
+  }
+`;
